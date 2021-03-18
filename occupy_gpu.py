c1cbd644f619988f3cd6fe55e8f16961b7f6318e
ZCC <zcuncun@outlook.com>
2021-03-18 16:57:00 +0800

    Create occupy_gpu.py

---
+++ occupy_gpu.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# Create by zcuncun @ 2021/03/18 11:08:26
+"""
+gpu_train.py
+"""
+
+import argparse, logging
+import torch
+import time
+import setproctitle
+
+setproctitle.setproctitle("gpu_train")
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--device",
+        type=str,
+        required=True,
+        help="device",
+    )
+
+    parser.add_argument(
+        "--time",
+        type=int,
+        required=True,
+        help="运行时间",
+    )
+
+    parser.add_argument(
+        "--size",
+        default=1,
+        type=int,
+        help="显存占用大小",
+    )
+
+    parser.add_argument(
+        "--usage",
+        default=1,
+        type=float,
+        help="使用率",
+    )
+
+    args = parser.parse_args()
+
+    train_device = "cuda:{}".format(args.device)
+    start_time = time.time()
+    sleep_time = 0
+    x = torch.rand([64, 3, 512, 512], device=train_device)
+    y = torch.rand([args.size*128, 3, 512, 512], device=train_device)
+    while True:
+        time.sleep(0.01)
+        for _ in range(int(10 * args.usage)):
+            torch.logit(x)
+        if args.time > 0 and time.time() - start_time > args.time:
+            break
+        
+
+        
+        
+
+if __name__ == "__main__":
+    main()
